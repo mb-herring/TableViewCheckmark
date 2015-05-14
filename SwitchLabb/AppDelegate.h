7feb2daddb78892abe4f07ef530af35ee1a9d11e
martinborstrand <martin@pinyagroup.com>
2015-05-14 15:58:59 +0200

    Initial Commit

---
+++ SwitchLabb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwitchLabb
+//
+//  Created by Martin Borstrand on 2015-05-14.
+//  Copyright (c) 2015 Martin Borstrand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwitchLabb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwitchLabb
+//
+//  Created by Martin Borstrand on 2015-05-14.
+//  Copyright (c) 2015 Martin Borstrand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
